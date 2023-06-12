8233cd4879773d5cf80f6e6ff64d929ac9062148
aditya piyushbhai siddhapura <techxpert.aditya2409@gmail.com>
2023-06-12 10:29:16 +0530

    the UI of task screen is done

--- lib/screens/tasks_screens.dart
+++ lib/screens/tasks_screens.dart
@@ -1,59 +1,108 @@
 import 'package:flutter/material.dart';
 
-class TasksScreen extends StatelessWidget {
+class TasksScreen extends StatefulWidget {
   const TasksScreen({super.key});
 
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  List<bool> checkedList = <bool>[false, false, false];
+  final List<String> tasks = <String>['be Good', 'be Brave', 'be Bold'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
-        child: Container(
-          padding:
-              const EdgeInsets.only(top: 100, left: 40, right: 40, bottom: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Material(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      child: Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Icon(
-                          Icons.list,
-                          color: Colors.lightBlueAccent,
-                          size: 60,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 0, right: 0, top: 40),
-                      child: Text(
-                        'Todoey',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(
-                      '12 tasks',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w200),
-                    ),
-                  ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // the top display of the app
+          Container(
+            padding: const EdgeInsets.only(top: 60, left: 40, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          //the list view of tasks
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
               ),
-              const Expanded(flex: 2, child: Material(child: Text('adi'))),
-            ],
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              tasks[index],
+                              style: TextStyle(
+                                  decoration: checkedList[index]
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 18),
+                            ),
+                          ),
+                          Checkbox(
+                            value: checkedList[index],
+                            onChanged: (bool? value) {
+                              setState(() {
+                                checkedList[index] = value ?? false;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
